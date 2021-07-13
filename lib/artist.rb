152d8a1fac97b290ba60c825bb6b431f0b685e53
scailles <scailles23@gmail.com>
2021-07-13 01:52:33 -0700

    commit tree 05cfa20a3d15082f5836060b2225a77a8971c562

---
+++ lib/artist.rb
@@ -0,0 +1,35 @@
+class Artist
+
+attr_accessor :name, :songs
+
+
+
+def initialize(name)
+    @name=name
+    @songs=[]
+end
+
+def songs
+    Song.all.select do |song|
+    song.artist == self
+    end
+end
+
+def add_song(song)
+    @songs << song
+    song.artist = self
+  
+end
+
+def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+    
+end
+
+def self.song_count
+ Song.all.count
+end
+
+end
